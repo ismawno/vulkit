0df4b338db9269f52cce75515bfca895de448825
Ismael Bueno <ismaelwno@gmail.com>
2024-12-06 01:03:26 +0100

    Add some more assertions

--- tests/tests/backend/instance.cpp
+++ tests/tests/backend/instance.cpp
@@ -10,5 +10,18 @@ TEST_CASE("Minimal headless instance", "[instance][headless]")
 
     const auto result = Instance::Builder().SetHeadless().Build();
     REQUIRE(result);
+
+    const Instance &instance = result.GetValue();
+    const Instance::Info &info = instance.GetInfo();
+
+    REQUIRE(!info.ApplicationName);
+    REQUIRE(!info.EngineName);
+    REQUIRE(info.ApplicationVersion == VKIT_MAKE_VERSION(0, 1, 0, 0));
+    REQUIRE(info.EngineVersion == VKIT_MAKE_VERSION(0, 1, 0, 0));
+    REQUIRE(info.ApiVersion == VKIT_MAKE_VERSION(0, 1, 0, 0));
+    REQUIRE(info.Flags & InstanceFlags_Headless);
+    REQUIRE(info.EnabledLayers.size() == 0);
+    REQUIRE(!info.DebugMessenger);
+    REQUIRE(!info.AllocationCallbacks);
 }
 } // namespace VKit
