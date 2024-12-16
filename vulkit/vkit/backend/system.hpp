c31ccc00a966e5ffa45b178a2feb32eefe1f11e7
Ismael Bueno <ismaelwno@gmail.com>
2024-12-16 17:11:18 +0100

    Add single quotes around string parsing

--- vulkit/vkit/backend/system.hpp
+++ vulkit/vkit/backend/system.hpp
@@ -31,9 +31,10 @@
 
 #ifdef TKIT_ENABLE_ASSERTS
 #    define VKIT_ASSERT_VULKAN_RESULT(result)                                                                          \
-        TKIT_ASSERT(result, "VkResult: {} - Onyx message: {}", VKit::VkResultToString(result.Result), result.Message)
+        TKIT_ASSERT(result, "VkResult: '{}' - Onyx message: '{}'", VKit::VkResultToString(result.Result),              \
+                    result.Message)
 #    define VKIT_ASSERT_RESULT(result)                                                                                 \
-        TKIT_ASSERT(result, "VkResult: {} - Onyx message: {}", VKit::VkResultToString(result.GetError().Result),       \
+        TKIT_ASSERT(result, "VkResult: '{}' - Onyx message: '{}'", VKit::VkResultToString(result.GetError().Result),   \
                     result.GetError().Message)
 #else
 #    define VKIT_ASSERT_VULKAN_RESULT(result) (void)result
@@ -42,11 +43,11 @@
 
 #ifdef TKIT_ENABLE_INFO_LOGS
 #    define VKIT_LOG_VULKAN_RESULT(result)                                                                             \
-        TKIT_LOG_INFO_IF(!result, "VkResult: {} - Onyx message: {}", VKit::VkResultToString(result.Result),            \
+        TKIT_LOG_INFO_IF(!result, "VkResult: '{}' - Onyx message: '{}'", VKit::VkResultToString(result.Result),        \
                          result.Message)
 #    define VKIT_LOG_RESULT(result)                                                                                    \
-        TKIT_LOG_INFO_IF(!result, "VkResult: {} - Onyx message: {}", VKit::VkResultToString(result.GetError().Result), \
-                         result.GetError().Message)
+        TKIT_LOG_INFO_IF(!result, "VkResult: '{}' - Onyx message: '{}'",                                               \
+                         VKit::VkResultToString(result.GetError().Result), result.GetError().Message)
 #else
 #    define VKIT_LOG_VULKAN_RESULT(result) (void)result
 #    define VKIT_LOG_RESULT(result)
@@ -54,10 +55,10 @@
 
 #ifdef TKIT_ENABLE_WARNING_LOGS
 #    define VKIT_WARN_VULKAN_RESULT(result)                                                                            \
-        TKIT_LOG_WARNING_IF(!result, "VkResult: {} - Onyx message: {}", VKit::VkResultToString(result.Result),         \
+        TKIT_LOG_WARNING_IF(!result, "VkResult: '{}' - Onyx message: '{}'", VKit::VkResultToString(result.Result),     \
                             result.Message)
 #    define VKIT_WARN_RESULT(result)                                                                                   \
-        TKIT_LOG_WARNING_IF(!result, "VkResult: {} - Onyx message: {}",                                                \
+        TKIT_LOG_WARNING_IF(!result, "VkResult: '{}' - Onyx message: '{}'",                                            \
                             VKit::VkResultToString(result.GetError().Result), result.GetError().Message)
 #else
 #    define VKIT_WARN_VULKAN_RESULT(result) (void)result
