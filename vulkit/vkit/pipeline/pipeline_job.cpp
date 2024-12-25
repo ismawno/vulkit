680bd957b37fd11c5f4090b44674b76307c03b0c
Ismael Bueno <ismaelwno@gmail.com>
2024-12-25 18:47:45 +0000

    Compute and graphics job merged into one because of their similarities

---
+++ vulkit/vkit/pipeline/pipeline_job.cpp
@@ -0,0 +1,74 @@
+#include "vkit/core/pch.hpp"
+#include "vkit/pipeline/pipeline_job.hpp"
+#include "vkit/descriptors/descriptor_set.hpp"
+
+namespace VKit
+{
+template <Pipeline Pip>
+IPipelineJob<Pip>::IPipelineJob(const ComputePipeline &p_Pipeline, const VkPipelineLayout p_Layout) noexcept
+    : m_Pipeline(p_Pipeline), m_Layout(p_Layout)
+{
+}
+
+template <Pipeline Pip> void IPipelineJob<Pip>::AddDescriptorSet(VkDescriptorSet p_DescriptorSet) noexcept
+{
+    m_DescriptorSets.push_back(p_DescriptorSet);
+}
+template <Pipeline Pip>
+void IPipelineJob<Pip>::UpdateDescriptorSet(u32 p_Index, VkDescriptorSet p_DescriptorSet) noexcept
+{
+    m_DescriptorSets[p_Index] = p_DescriptorSet;
+}
+
+template <Pipeline Pip>
+void IPipelineJob<Pip>::Bind(const VkCommandBuffer p_CommandBuffer, u32 p_FirstSet,
+                             const std::span<const u32> p_DynamicOffsets) const noexcept
+{
+    m_Pipeline.Bind(p_CommandBuffer);
+
+    if constexpr (std::is_same_v<Pip, GraphicsPipeline>)
+        DescriptorSet::Bind(p_CommandBuffer, m_DescriptorSets, VK_PIPELINE_BIND_POINT_GRAPHICS, m_Layout, p_FirstSet,
+                            p_DynamicOffsets);
+    else
+        DescriptorSet::Bind(p_CommandBuffer, m_DescriptorSets, VK_PIPELINE_BIND_POINT_COMPUTE, m_Layout, p_FirstSet,
+                            p_DynamicOffsets);
+
+    u32 offset = 0;
+    const u32 pushCount = static_cast<u32>(m_PushData.size());
+
+    // Data may not need to be pushed every frame... but I guess it is a small price to pay for the flexibility
+    for (u32 i = 0; i < pushCount; ++i)
+    {
+        const PushDataInfo &info = m_PushData[i];
+        vkCmdPushConstants(p_CommandBuffer, m_Layout, VK_SHADER_STAGE_COMPUTE_BIT, offset, info.Size, info.Data);
+        offset += info.Size;
+    }
+}
+template <Pipeline Pip> IPipelineJob<Pip>::operator bool() const noexcept
+{
+    return m_Pipeline;
+}
+
+void PipelineJob<ComputePipeline>::Dispatch(const VkCommandBuffer p_CommandBuffer, const u32 p_GroupCountX,
+                                            const u32 p_GroupCountY, const u32 p_GroupCountZ) const noexcept
+{
+    vkCmdDispatch(p_CommandBuffer, p_GroupCountX, p_GroupCountY, p_GroupCountZ);
+}
+
+void PipelineJob<GraphicsPipeline>::Draw(const VkCommandBuffer p_CommandBuffer, const u32 p_VertexCount,
+                                         const u32 p_InstanceCount, const u32 p_FirstVertex,
+                                         const u32 p_FirstInstance) noexcept
+{
+    vkCmdDraw(p_CommandBuffer, p_VertexCount, p_InstanceCount, p_FirstVertex, p_FirstInstance);
+}
+void PipelineJob<GraphicsPipeline>::DrawIndexed(const VkCommandBuffer p_CommandBuffer, const u32 p_IndexCount,
+                                                const u32 p_InstanceCount, const u32 p_FirstIndex,
+                                                const i32 p_VertexOffset, const u32 p_FirstInstance) noexcept
+{
+    vkCmdDrawIndexed(p_CommandBuffer, p_IndexCount, p_InstanceCount, p_FirstIndex, p_VertexOffset, p_FirstInstance);
+}
+
+template class IPipelineJob<ComputePipeline>;
+template class IPipelineJob<GraphicsPipeline>;
+
+} // namespace VKit
---
+++ vulkit/vkit/pipeline/pipeline_job.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "vkit/pipeline/compute_pipeline.hpp"
+#include "vkit/pipeline/graphics_pipeline.hpp"
+#include "vkit/pipeline/compute_pipeline.hpp"
+
+namespace VKit
+{
+template <typename T>
+concept Pipeline = std::is_same_v<T, GraphicsPipeline> || std::is_same_v<T, ComputePipeline>;
+
+template <Pipeline Pip> constexpr VkShaderStageFlags DefaultShaderStage() noexcept
+{
+    if constexpr (std::is_same_v<Pip, GraphicsPipeline>)
+        return VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+    else
+        return VK_SHADER_STAGE_COMPUTE_BIT;
+}
+
+template <Pipeline Pip> class VKIT_API IPipelineJob
+{
+  public:
+    IPipelineJob() noexcept = default;
+    IPipelineJob(const ComputePipeline &p_Pipeline, VkPipelineLayout p_Layout) noexcept;
+
+    void AddDescriptorSet(VkDescriptorSet p_DescriptorSet) noexcept;
+    void UpdateDescriptorSet(u32 p_Index, VkDescriptorSet p_DescriptorSet) noexcept;
+
+    template <typename T>
+    void AddPushConstantRange(const T *p_Data, const VkShaderStageFlags p_Stages = DefaultShaderStage<Pip>()) noexcept
+    {
+        m_PushData.push_back(PushDataInfo{p_Data, sizeof(T), p_Stages});
+    }
+    template <typename T>
+    void UpdatePushConstantRange(u32 p_Index, const T *p_Data,
+                                 const VkShaderStageFlags p_Stages = DefaultShaderStage<Pip>()) noexcept
+    {
+        m_PushData[p_Index] = PushDataInfo{p_Data, sizeof(T), p_Stages};
+    }
+
+    void Bind(VkCommandBuffer p_CommandBuffer, u32 p_FirstSet = 0,
+              std::span<const u32> p_DynamicOffsets = {}) const noexcept;
+
+    explicit(false) operator bool() const noexcept;
+
+  private:
+    struct PushDataInfo
+    {
+        const void *Data = nullptr;
+        u32 Size = 0;
+        VkShaderStageFlags Stages = 0;
+    };
+
+    ComputePipeline m_Pipeline{};
+    VkPipelineLayout m_Layout = VK_NULL_HANDLE;
+    TKit::StaticArray8<VkDescriptorSet> m_DescriptorSets;
+    TKit::StaticArray4<PushDataInfo> m_PushData;
+};
+
+template <Pipeline Pip> class PipelineJob;
+
+template <> class VKIT_API PipelineJob<GraphicsPipeline> final : public IPipelineJob<GraphicsPipeline>
+{
+  public:
+    using IPipelineJob<GraphicsPipeline>::IPipelineJob;
+
+    void Draw(VkCommandBuffer p_CommandBuffer, u32 p_VertexCount, u32 p_InstanceCount = 1, u32 p_FirstVertex = 0,
+              u32 p_Firstinstance = 0) noexcept;
+    void DrawIndexed(VkCommandBuffer commandBuffer, u32 p_IndexCount, u32 p_InstanceCount = 1, u32 p_FirstIndex = 0,
+                     i32 p_VertexOffset = 0, u32 p_FirstInstance = 0) noexcept;
+};
+
+template <> class VKIT_API PipelineJob<ComputePipeline> final : public IPipelineJob<ComputePipeline>
+{
+  public:
+    using IPipelineJob<ComputePipeline>::IPipelineJob;
+
+    void Dispatch(VkCommandBuffer p_CommandBuffer, u32 p_GroupCountX, u32 p_GroupCountY,
+                  u32 p_GroupCountZ) const noexcept;
+};
+
+using GraphicsJob = PipelineJob<GraphicsPipeline>;
+using ComputeJob = PipelineJob<ComputePipeline>;
+
+} // namespace VKit
