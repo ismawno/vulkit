2e343c53fafadc6b26da92448c0cbdc3d6124d47
Ismael Bueno <ismaelwno@gmail.com>
2024-12-31 19:54:47 +0000

    Fix some inconsistencies with cmake scanner

---
+++ setup/build.py
@@ -0,0 +1,150 @@
+from pathlib import Path
+from argparse import ArgumentParser, Namespace
+from configparser import ConfigParser
+
+import subprocess
+import os
+
+
+def try_convert_bool(val: str) -> bool | str:
+    if val == "True":
+        return True
+    elif val == "False":
+        return False
+    return val
+
+
+def parse_build_ini() -> dict[str, tuple[str, str | bool]]:
+    cfg = ConfigParser()
+
+    root = Path(__file__).parent
+    path = root / "build.ini"
+    if not path.exists():
+        raise FileNotFoundError(f"No 'build.ini' file found in {root}")
+
+    with open(path) as f:
+        cfg.read_file(f)
+
+    parsed = {}
+    for ogvarname, kv in cfg["default-values"].items():
+        varname, val = kv.split(": ")
+        if varname in parsed:
+            raise ValueError(
+                f"Name mismatch! Variable '{varname}' already exists in the parsed dictionary"
+            )
+
+        parsed[ogvarname] = (varname, try_convert_bool(val))
+
+    return parsed
+
+
+def parse_arguments(
+    arguments: dict[str, tuple[str, str | bool]]
+) -> tuple[Namespace, dict[str, str | bool]]:
+    desc = """
+    This script takes in a .ini configuration file created from cmake_scanner.py and
+    runs CMake with the options specified in the configuration file, avoiding cache shit.
+
+    This file must be in the same directory as build.ini file from where create the building arguments.
+    """
+
+    parser = ArgumentParser(description=desc)
+    parser.add_argument(
+        "-b",
+        "--build",
+        default=Path("build"),
+        type=Path,
+        help="The location where the project will be built.",
+    )
+    parser.add_argument(
+        "-s",
+        "--source",
+        default=Path("."),
+        type=Path,
+        help="The location where the source files are found (where the root CMakeLists.txt is).",
+    )
+    parser.add_argument(
+        "--disable-toolkit-conditional-logs",
+        action="store_true",
+        default=False,
+        help="Disable conditional logs for the toolkit package if exists. By default, toolkit logging will only be enabled on Dist builds. Disabling this option removes this behavior. Logging can still be enabled by entering the argument in the command line no matter what.",
+    )
+
+    arg_map = {}
+    for ogvarname, (varname, val) in arguments.items():
+        if isinstance(val, bool):
+            cmakeval = "ON" if val else "OFF"
+            parser.add_argument(
+                f"--{varname}",
+                action="store_true",
+                default=False,
+                help=f"Ensures that the CMake option '{ogvarname.upper()}' is set of 'ON'. Its default is '{cmakeval}'.",
+            )
+            parser.add_argument(
+                f"--no-{varname}",
+                action="store_true",
+                default=False,
+                help=f"Ensures that the CMake option '{ogvarname.upper()}' is set of 'OFF'. Its default is '{cmakeval}'.",
+            )
+        else:
+            parser.add_argument(
+                f"--{varname}",
+                type=str,
+                default=val,
+                help=f"Set the value of '{ogvarname.upper()}' to the specified value. Default is '{val}'.",
+            )
+
+        arg_map[varname.replace("-", "_")] = ogvarname, val
+    return parser.parse_args(), arg_map
+
+
+def main() -> None:
+    arguments = parse_build_ini()
+    args, arg_map = parse_arguments(arguments)
+
+    build_path: Path = args.build.resolve()
+    source_path: Path = args.source.resolve()
+
+    cnd_logs = not args.disable_toolkit_conditional_logs
+    arg_dict = vars(args)
+
+    cmake_args = {}
+    for k, v in arg_dict.items():
+        if k not in arg_map:
+            continue
+
+        ogvarname, default = arg_map[k]
+        if not isinstance(v, bool):
+            cmake_args[f"-D{ogvarname.upper()}"] = v.replace('"', "")
+            continue
+
+        nv = arg_dict[f"no_{k}"]
+        if v and nv:
+            raise ValueError(
+                f"Cannot set both '{k}' and 'no_{k}' to True. Please choose one"
+            )
+        if v:
+            cmake_args[f"-D{ogvarname.upper()}"] = "ON"
+        elif nv:
+            cmake_args[f"-D{ogvarname.upper()}"] = "OFF"
+        elif cnd_logs and (
+            "log" in ogvarname.lower() or "enable_asserts" in ogvarname.lower()
+        ):
+            btype = arguments["cmake_build_type"][1]
+            cmake_args[f"-D{ogvarname.upper()}"] = "ON" if btype != "Dist" else "OFF"
+        else:
+            cmake_args[f"-D{ogvarname.upper()}"] = "ON" if default else "OFF"
+
+    cmake_args = [f"{k}={v}" for k, v in cmake_args.items()]
+    build_path.mkdir(exist_ok=True, parents=True)
+
+    print("Running CMake with the following arguments:")
+    for arg in cmake_args:
+        print(f"    {arg}")
+
+    os.chdir(build_path)
+    subprocess.run(["cmake", str(source_path)] + cmake_args, check=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup/cmake_scanner.py
@@ -0,0 +1,156 @@
+from pathlib import Path
+from argparse import ArgumentParser, Namespace
+from configparser import ConfigParser
+
+
+def parse_arguments() -> Namespace:
+    desc = """
+    This is a CMake scanner whose sole purpose is to avoid the evil thing that is CMake's cache.
+    When running a CMake script, I would like to know what the script is doing, and what it is going to do.
+    This script will scan the CMakeLists.txt file and detect configuration options, so that it can
+    parse them and specify them explicitly to nullify the effects of the cache, so that I have no surprises.
+    
+    This script will create a build.ini file in the same directory this file lives, to be used by build.py to do
+    the actual building.
+    """
+
+    parser = ArgumentParser(description=desc)
+    parser.add_argument(
+        "-p",
+        "--path",
+        type=Path,
+        required=True,
+        help="The path to search for CMakelists.txt.",
+    )
+    parser.add_argument(
+        "--hint",
+        default="define_option",
+        type=str,
+        help="A hint to let the scanner now how to look for options",
+    )
+    parser.add_argument(
+        "--keep-preffixes",
+        action="store_true",
+        default=True,
+        help="Keep the preffixes in the option name. Default is to strip them.",
+    )
+    parser.add_argument(
+        "--preffixes",
+        action="append",
+        # Make sure largest preffixes are first
+        default=[
+            "CMAKE_",
+            "TOOLKIT_ENABLE_",
+            "VULKIT_ENABLE_",
+            "ONYX_ENABLE_",
+            "TOOLKIT_",
+            "VULKIT_",
+            "ONYX_",
+        ],
+        help="The preffixes to strip.",
+    )
+    parser.add_argument(
+        "--keep-case",
+        action="store_true",
+        default=False,
+        help="Keep the case of the option name. Default is to lowercase it.",
+    )
+    parser.add_argument(
+        "--keep-underscore",
+        action="store_true",
+        default=False,
+        help="Keep the underscore in the option name. Default is to replace it with a hyphen.",
+    )
+
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_arguments()
+    hint: str = args.hint
+    cmake_path: Path = args.path
+    strip_preffix: bool = not args.keep_preffixes
+    preffixes: list[str] = args.preffixes
+    lower_case: bool = not args.keep_case
+    dyphen_separator: bool = not args.keep_underscore
+
+    def process_ogvarname(ogvarname: str, override_disable_strip: bool = False) -> str:
+        varname = ogvarname
+        if strip_preffix and not override_disable_strip:
+            for preffix in preffixes:
+                if varname.startswith(preffix):
+                    varname = varname.removeprefix(preffix)
+        if lower_case:
+            varname = varname.lower()
+        if dyphen_separator:
+            varname = varname.replace("_", "-")
+        return varname
+
+    def process_option(content: list[str]) -> tuple[str, str, str | bool]:
+        ogvarname, val = content
+        print(f"    Detected option '{ogvarname}' with default value '{val}'")
+
+        varname = process_ogvarname(ogvarname)
+
+        if val == "ON":
+            val = True
+        elif val == "OFF":
+            val = False
+
+        return ogvarname, varname, val
+
+    contents = []
+    for cmake_file in cmake_path.rglob("CMakeLists.txt"):
+        print(
+            f"Scanning file at '{cmake_file}'. Looking for lines starting with '{hint}'..."
+        )
+        with open(cmake_file, "r") as f:
+            options = [
+                process_option(
+                    content.removeprefix(f"{hint}(").removesuffix(")").split(" ")
+                )
+                for content in f.read().splitlines()
+                if content.startswith(hint)
+            ]
+            contents.extend(options)
+        if not options:
+            print("    Nothing found...")
+
+    contents = {content[0]: (content[1], content[2]) for content in contents}
+    unique_varnames = {}
+
+    print("")
+    for ogvarname, (varname, val) in contents.items():
+        if varname not in unique_varnames:
+            unique_varnames[varname] = ogvarname
+            continue
+        print(
+            f"Warning: Found name clash with '{varname}'. Trying to resolve by restoring preffixes..."
+        )
+        if not strip_preffix:
+            raise ValueError(
+                f"Name clash with '{varname}' that was not caused by preffix stripping. Aborting..."
+            )
+        other_ogvarname = unique_varnames[varname]
+        contents[other_ogvarname] = (
+            process_ogvarname(other_ogvarname, override_disable_strip=True),
+            contents[other_ogvarname][1],
+        )
+        contents[ogvarname] = (
+            process_ogvarname(ogvarname, override_disable_strip=True),
+            contents[ogvarname][1],
+        )
+        print("Resolved!")
+
+    cfg = ConfigParser()
+    cfg.add_section("default-values")
+    for ogvarname, (varname, val) in contents.items():
+        cfg["default-values"][ogvarname] = f"{varname}: {val}"
+
+    path = Path(__file__).parent
+    with open(path / "build.ini", "w") as f:
+        cfg.write(f)
+
+
+if __name__ == "__main__":
+    main()
