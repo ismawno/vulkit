adfe10bcd705527202924b1ae5fd13c7af68623a
Ismael Bueno <ismaelwno@gmail.com>
2025-12-05 18:54:01 +0100

    chore: Update to nix 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Dev shell for vulkit";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs =
